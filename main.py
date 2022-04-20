afaa204372f21770afab2bd2d0d1b9c9671a14b8
StudyAnkit <ankitghosh2018@gmail.com>
2022-04-20 15:43:21 +0530

    [Ankit]- Attendance Calculation

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import random
+
 print("Welcome to Employee Wage Computation")
+
+
+# UC1 - Employee Attendance
+def Attendance():
+    attendance_check = random.randint(0, 2)
+    if attendance_check == 1:
+        print("Employee is present")
+    else:
+        print("Employee is absent")
+
+
+print(Attendance())
